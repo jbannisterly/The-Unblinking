1d256540343195ba6473e8babb249309f8bd775b
jbannisterly <jbannisterly@gmail.com>
2025-02-08 22:03:49 +0000

    title screen

---
+++ Scripts/cutscene.gd
@@ -0,0 +1,16 @@
+extends VideoStreamPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_texture_rect_gui_input(event: InputEvent) -> void:
+	if event.is_pressed():
+		play()
